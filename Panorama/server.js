ae831a048bfb868b845cbd7356e9fd0ad14a5895
HamzaSa11 <hamza.saoudi.850@gmail.com>
2025-12-24 03:22:58 +0100

    Fix: correct static file path from Panorama to Panorama/Panorama

--- Panorama/server.js
+++ Panorama/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(bodyParser.json({ limit: '10kb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10kb' }));
-app.use(express.static('Panorama'));
+app.use(express.static('Panorama/Panorama'));
 
 const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 const validatePhone = (phone) => /^[0-9\s\-\+\(\)]{8,}$/.test(phone);
