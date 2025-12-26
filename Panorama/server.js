240d7b0384edacb2397473d21ee880ebe6ee627f
HamzaSa11 <hamza.saoudi.850@gmail.com>
2025-12-26 16:21:01 +0100

    Update server.js

--- Panorama/server.js
+++ Panorama/server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
   res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   res.setHeader('X-XSS-Protection', '1; mode=block');
   res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-  res.setHeader('Content-Security-Policy', "default-src 'self' https://unpkg.com https://*.tile.openstreetmap.org; script-src 'self' 'unsafe-inline' https://unpkg.com; style-src 'self' 'unsafe-inline' https://unpkg.com; img-src 'self' https: data:; font-src 'self' data:; connect-src 'self' https://*.tile.openstreetmap.org https://unpkg.com;");
+  res.setHeader('Content-Security-Policy', "default-src 'self' https://maps.googleapis.com https://maps.gstatic.com; script-src 'self' 'unsafe-inline' https://maps.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' https: data:; font-src 'self' data:");
   next();
 });
 
